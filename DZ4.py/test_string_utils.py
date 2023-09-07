7eb09c315e362922aa3273d107fca091e93278c3
Artem <132493257+ArtemSochi29@users.noreply.github.com>
2023-09-07 20:36:35 +0300

    ДЗ4 готово

--- DZ4.py/test_string_utils.py
+++ DZ4.py/test_string_utils.py
@@ -24,3 +24,37 @@ def test_to_list(str, delimeter, result):
     res = stringUtils.to_list(str, delimeter)
     assert res == result
 
+@pytest.mark.parametrize('str, str1, result', [ ("Asrtem", "s", "Artem"), ("OkArtem", "Ok", "Artem") ])
+
+def test_delete(str, str1, result):
+    stringUtils = StringUtils()
+    res = stringUtils.delete_symbol(str, str1)
+    assert res == result
+
+@pytest.mark.parametrize('str, str1, result', [("Artem", "A", True), ("Artem", "t", False)])
+
+def test_starts_with(str, str1, result):
+    stringUtils = StringUtils()
+    res = stringUtils.starts_with(str, str1)
+    assert res == result
+
+@pytest.mark.parametrize('str, str1, result', [("Artem", "m", True), ("Artem", "t", False)])
+
+def test_end_with(str, str1, result):
+    stringUtils = StringUtils()
+    res = stringUtils.end_with(str, str1)
+    assert res == result
+
+@pytest.mark.parametrize('str, result', [("", True), ("   ", True), ("Artem", False)])
+
+def test_is_empty(str, result):
+    stringUtils = StringUtils()
+    res = stringUtils.is_empty(str)
+    assert res == result
+
+@pytest.mark.parametrize('lst, joiner, result', [("1,2,3,4", ",", ["1,2,3,4"] ), ("Artem, Bas", ",","[Artem, Bas]"), ("Bas", "-", "B-a-s" )])
+
+def test_list_to_string(lst, joiner, result):
+    stringUtils = StringUtils()
+    res = stringUtils.list_to_string(lst, joiner)
+    assert res == result
