5e44bd51d02ca78f420628437bb2c68399f0374a
Artem <132493257+ArtemSochi29@users.noreply.github.com>
2023-10-18 11:06:09 +0300

    Доработка 3 задания

--- DZ6.py/3_wait_for_picture.py
+++ DZ6.py/3_wait_for_picture.py
@@ -1,4 +1,4 @@
-from time import sleep
+#from time import sleep
 from selenium.webdriver.common.by import By   
 from selenium import webdriver  
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,16 +7,9 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
-sleep(10)
 
-waiter = WebDriverWait(driver, 20)
+wait = WebDriverWait(driver, 20)
 
-waiter.until(
-    EC.element_to_be_selected((By.CSS_SELECTOR, "#award"), "srt")
-)
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#award")))
 
-print(waiter)
-
-
-
-
+print(driver.find_element(By.CSS_SELECTOR, "#award").get_dom_attribute("src"))
